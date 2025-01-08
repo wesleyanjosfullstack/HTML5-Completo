4c2d2d3cda702b7b38efb689bcd088dd1dcd012b
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2025-01-08 12:14:40 -0300

    FEAT - Testando eventos no formulario com js

--- Login/js/script.js
+++ Login/js/script.js
@@ -1,1 +1,19 @@
-console.log('em uso por wesley')
+const email = 'wesleyanjosfullstack@gmail.com'
+console.log(`Seu endereço de e-mail é ${email}`)
+
+document.getElementById('btn-submit').addEventListener('click', e => {
+    console.log('Clique no botão !')
+})
+
+document.getElementById('form-login').addEventListener('mouseenter', e => {
+    console.log('Mouse sobre o formulário !')
+})
+
+document.getElementById('form-login').addEventListener('mouseleave', e => {
+    console.log('Mouse saiu do formulário !')
+})
+
+document.getElementById('form-login').addEventListener('submit', e => {
+    e.preventDefault()
+    console.log('Formulário enviado ! Aqui vai o AJAX !')
+})
