dfcbf7014d283b421e8de9c323c9a4a7ce912d27
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2025-01-08 14:17:37 -0300

    FEAT - Tabuada calculando com js
    Adicionando a função calcTab para criar e calcular os valores da tabeba e adicionando um campo para digitar o usuário digitar

--- Tabuada/js/script.js
+++ Tabuada/js/script.js
@@ -1,19 +1,25 @@
-let tabuada = document.querySelector('#tabuada tbody')
-let valueA = 100
-tabuada.innerHTML = ''
-
-for (let valueB = 0; valueB <= 10; valueB++) {
-    let result = valueA * valueB
-    let template = `
-                <tr>
-                    <td>${valueA}</td>
-                    <td>X</td>
-                    <td>${valueB}</td>
-                    <td>=</td>
-                    <td>${result}</td>
-                </tr>
-    `
-    let tr = document.createElement('tr')
-    tr.innerHTML = template
-    tabuada.append(tr)
+function calcTab() {
+    let tabuada = document.querySelector('#tabuada tbody')
+    let valueA = parseInt(document.getElementById('valueA').value)
+    tabuada.innerHTML = ''
+    
+    for (let valueB = 0; valueB <= 10; valueB++) {
+        let result = valueA * valueB
+        let template = `
+                    <tr>
+                        <td>${valueA}</td>
+                        <td>X</td>
+                        <td>${valueB}</td>
+                        <td>=</td>
+                        <td>${result}</td>
+                    </tr>
+        `
+        let tr = document.createElement('tr')
+        tr.innerHTML = template
+        tabuada.append(tr)
+    }
 }
+
+calcTab()
+
+document.getElementById('valueA').addEventListener('change', calcTab)
