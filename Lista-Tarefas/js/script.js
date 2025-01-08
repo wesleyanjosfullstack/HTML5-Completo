e55bfdee33e8df40f3aa76e158b7dbba6f017a38
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2025-01-08 14:41:21 -0300

    FEAT - Alimento a lista de tarefa com array no js

--- Lista-Tarefas/js/script.js
+++ Lista-Tarefas/js/script.js
@@ -1,10 +1,25 @@
-let data = [{
-    id: 1,
-    title: 'Estudar HTML5'
-}, {
-    id: 2,
-    title: 'Estudar CSS3'
-}, {
-    id: 3,
-    title: 'Estudar JS'
-}]
+let data = [
+    {
+        id: 1,
+        title: 'Estudar HTML5'
+    }, {
+        id: 2,
+        title: 'Estudar CSS3'
+    }, {
+        id: 3,
+        title: 'Estudar JS'
+    }, {
+        id: 4,
+        title: 'Estudar PHP'
+    }
+]
+
+data.forEach(task => {
+    let li = document.createElement('li')
+    
+    li.innerHTML = `
+        <input type="checkbox" id="task-${task.id}"><label for="${task.id}">${task.title}</label></input>
+    `
+    
+    document.querySelector('.to-do').append(li)
+})
