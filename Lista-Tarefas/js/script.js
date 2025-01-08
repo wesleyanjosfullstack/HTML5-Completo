025fa780ccacb9214dbfdf7efbbb1117d9432b63
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2025-01-08 14:58:30 -0300

    FEAT - Adicionando elemento na lista

--- Lista-Tarefas/js/script.js
+++ Lista-Tarefas/js/script.js
@@ -1,34 +1,34 @@
-let data = [
-    {
-        id: 1,
-        title: 'Estudar HTML5'
-    }, {
-        id: 2,
-        title: 'Estudar CSS3'
-    }, {
-        id: 3,
-        title: 'Estudar JS'
-    }, {
-        id: 4,
-        title: 'Estudar PHP'
-    }
-]
+let data = []
 
-data.forEach(task => {
-    let li = document.createElement('li')
+function renderTodo() {
+    document.querySelector('.to-do').innerHTML = ''
+
+    data.forEach(task => {
+        let li = document.createElement('li')
+        
+        li.innerHTML = `
+            <input type="checkbox" id="task-${task.id}"><label for="${task.id}">${task.title}</label></input>
+        `
     
-    li.innerHTML = `
-        <input type="checkbox" id="task-${task.id}"><label for="${task.id}">${task.title}</label></input>
-    `
-
-    li.querySelector('input').addEventListener('change', e => {
-        if (e.target.checked) li.classList.add('complete')
-        else li.classList.add('remove')
+        li.querySelector('input').addEventListener('change', e => {
+            if (e.target.checked) li.classList.add('complete')
+            else li.classList.add('remove')
+        })
+        
+        document.querySelector('.to-do').append(li)
     })
-    
-    document.querySelector('.to-do').append(li)
-})
+}
 
 document.getElementById('new-task').addEventListener('keyup', e => {
-    if (e.key === 'Enter') console.log(e.target.value)
+    if (e.key === 'Enter') {
+        data.push({
+            id: data.length++,
+            title: e.target.value
+        })
+
+        e.target.value = ''
+        renderTodo()
+    }
 })
+
+renderTodo()
